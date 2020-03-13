05e3ddf9a18882eb0dcf9d61a41d2e04335e0ecc
Lisa Garvey <garveygeneral@gmail.com>
2020-03-12 18:48:45 -0700

    update scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,14 @@
+//user interface
 $(document).ready(function() {
   $("form#input_return").submit(function(event) {
     event.preventDefault();
-    var userInput = parseInt($("input#number").val());
     
-    if (number.indexOf(3) !== -1) {
-      result="Won't you be my neighbor?";
-    } else if (number.indexOf(2) !== -1) {
-      result="Boop";
-    } else if (number.indexOf(1) !== -1) {
-      result="Beep!";
-    } else {
-      result();
+    var userInput = parseInt($("input#number").val());
+    var text = "";
+    for (var index = 0; index = userInput.length; index +=1); {
+      text += userInput[index] + ", ";
     }
-
-    $("#results").show(); 
+    $("#results").text(results); 
 
   });
 }); 
